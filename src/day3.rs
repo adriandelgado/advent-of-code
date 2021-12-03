288e7b05c3e2a00bf90df79af06c99ccac375895
Adrian Delgado <ajdm_3000@hotmail.com>
2021-12-03 02:44:09 -0500

    Day 3

---
+++ src/day3.rs
@@ -0,0 +1,119 @@
+use std::io::{BufRead, BufReader};
+
+const LINE_LEN: usize = 12;
+
+pub fn day3a() -> String {
+    let file = std::fs::read_to_string("./files/day3a.txt").unwrap();
+
+    let mut sum = [0; LINE_LEN];
+
+    for line in file.lines() {
+        assert_eq!(line.len(), LINE_LEN);
+
+        for (idx, chr) in line.bytes().enumerate() {
+            match chr {
+                b'0' => sum[idx] -= 1,
+                b'1' => sum[idx] += 1,
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    let gamma = sum
+        .iter()
+        .rev()
+        .map(|&d| i64::from(d > 0))
+        .enumerate()
+        .fold(0, |acc, (idx, digit)| {
+            let idx: u8 = idx.try_into().unwrap();
+            // acc + 2_i64.pow(idx) * digit
+            acc + (digit << idx)
+        });
+    let epsilon = sum
+        .iter()
+        .rev()
+        .map(|&d| i64::from(d < 0))
+        .enumerate()
+        .fold(0, |acc, (idx, digit)| {
+            let idx: u8 = idx.try_into().unwrap();
+            // acc + 2_i64.pow(idx) * digit
+            acc + (digit << idx)
+        });
+
+    (gamma * epsilon).to_string()
+}
+
+pub fn day3b() -> String {
+    let file = std::fs::File::open("./files/day3a.txt").unwrap();
+    let mut oxigen: Vec<_> = BufReader::new(file)
+        .lines()
+        .map(|line| line.unwrap().into_bytes())
+        .collect();
+    let mut co2 = oxigen.clone();
+    for idx in 0..LINE_LEN {
+        let mut sum_oxigen = 0;
+        let mut sum_co2 = 0;
+
+        for line in oxigen.iter() {
+            sum_oxigen += match line[idx] {
+                b'0' => -1,
+                b'1' => 1,
+                _ => unreachable!(),
+            }
+        }
+        for line in co2.iter() {
+            sum_co2 += match line[idx] {
+                b'0' => -1,
+                b'1' => 1,
+                _ => unreachable!(),
+            }
+        }
+
+        let digit_oxigen = if sum_oxigen < 0 { b'0' } else { b'1' };
+        let digit_co2 = if sum_co2 < 0 { b'1' } else { b'0' };
+
+        if oxigen.len() > 1 {
+            oxigen.retain(|line| line[idx] == digit_oxigen);
+        }
+        if co2.len() > 1 {
+            co2.retain(|line| line[idx] == digit_co2);
+        }
+    }
+
+    let oxigen_rating = oxigen[0]
+        .iter()
+        .map(|ch| match ch {
+            b'0' => 0,
+            b'1' => 1,
+            _ => unreachable!(),
+        })
+        .rev()
+        .enumerate()
+        .fold(0, |acc, (idx, digit)| {
+            let idx: u8 = idx.try_into().unwrap();
+            // acc + 2_i64.pow(idx) * digit
+            acc + (digit << idx)
+        });
+    let co2_rating = co2[0]
+        .iter()
+        .map(|ch| match ch {
+            b'0' => 0,
+            b'1' => 1,
+            _ => unreachable!(),
+        })
+        .rev()
+        .enumerate()
+        .fold(0, |acc, (idx, digit)| {
+            let idx: u8 = idx.try_into().unwrap();
+            // acc + 2_i64.pow(idx) * digit
+            acc + (digit << idx)
+        });
+
+    (oxigen_rating * co2_rating).to_string()
+}
+
+#[test]
+fn day3_is_correct() {
+    assert_eq!(day3a(), "3912944");
+    assert_eq!(day3b(), "1488311643");
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::env;
 use aoc::{
     day1::{day1a, day1b},
     day2::{day2a, day2b},
+    day3::{day3a, day3b},
 };
 
 fn main() {
@@ -15,6 +16,8 @@ fn main() {
         Some("1b") => day1b(),
         Some("2a") => day2a(),
         Some("2b") => day2b(),
+        Some("3a") => day3a(),
+        Some("3b") => day3b(),
         None => "Please input a day to solve".to_string(),
         _ => "Not solved yet".to_string(),
     };
