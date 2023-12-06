8b38a6c1033b922642449094487780190a57bc73
Adrian Delgado <git@adrianjdelgado.com>
2023-12-06 01:27:34 -0500

    Use math

--- src/y2023/d06.rs
+++ src/y2023/d06.rs
@@ -13,13 +13,27 @@ pub fn part1(input: &str) -> usize {
         .product()
 }
 
-pub fn part2(input: &str) -> usize {
+pub fn part2(input: &str) -> u64 {
     let (t, d) = extract_info_2.parse(input).unwrap();
 
-    (1..=t).filter(|hold| (t - hold) * hold > d).count()
+    amount_ways_beating((t, d))
 }
 
-fn extract_info_1(input: &mut &str) -> PResult<(Vec<u32>, Vec<u32>)> {
+#[allow(
+    clippy::cast_sign_loss,
+    clippy::cast_possible_truncation,
+    clippy::cast_precision_loss
+)]
+fn amount_ways_beating((t, d): (u64, u64)) -> u64 {
+    let (t, d) = (t as f64, d as f64);
+    let discriminant = t * t - 4.0 * d;
+    let start = (t - f64::sqrt(discriminant)) / 2.0;
+    let end = (t + f64::sqrt(discriminant)) / 2.0;
+
+    ((end - 1.0).ceil() - (start + 1.0).floor() + 1.0) as u64
+}
+
+fn extract_info_1(input: &mut &str) -> PResult<(Vec<u64>, Vec<u64>)> {
     terminated(
         separated_pair(get_several_nums, '\n', get_several_nums),
         rest,
@@ -27,10 +41,10 @@ fn extract_info_1(input: &mut &str) -> PResult<(Vec<u32>, Vec<u32>)> {
     .parse_next(input)
 }
 
-fn get_several_nums(input: &mut &str) -> PResult<Vec<u32>> {
+fn get_several_nums(input: &mut &str) -> PResult<Vec<u64>> {
     preceded(
         (alpha1, ":", space1),
-        separated(4, dec_uint::<_, u32, _>, space1),
+        separated(4, dec_uint::<_, u64, _>, space1),
     )
     .parse_next(input)
 }
