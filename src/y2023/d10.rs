99150d5ae96f7e1a713cc68968b8398b89410455
Adrian Delgado <git@adrianjdelgado.com>
2023-12-10 17:32:20 -0500

    Optimize Day 10

--- src/y2023/d10.rs
+++ src/y2023/d10.rs
@@ -1,135 +1,116 @@
-use std::collections::{btree_map::Entry, BTreeMap, BTreeSet};
+#![allow(
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_sign_loss
+)]
 
-use bstr::ByteSlice;
-use itertools::Itertools;
+use std::ops::Index;
 
-pub fn part1(input: &str) -> i32 {
-    let grid: Vec<_> = input.lines().map(|row| row.as_bytes().to_vec()).collect();
-    let row = grid.iter().position(|row| row.contains(&b'S')).unwrap();
-    let col = grid[row].find_byte(b'S').unwrap();
-    let row_len = grid[0].len();
-    let col_len = grid.len();
+pub fn part1(input: &str) -> u16 {
+    let grid = Grid::from_str(input).unwrap();
+    let (start_row, start_col) = grid.find(b'S').unwrap();
+    let (mut last_row, mut last_col) = grid.get_s_neighbors((start_row, start_col)).next().unwrap();
+    let (mut curr_row, mut curr_col) = (start_row, start_col);
 
-    let mut stack = vec![(row, col)];
-    let mut distances = BTreeMap::new();
-    distances.insert((row, col), 0);
-
-    while let Some(pipe) = stack.pop() {
-        let curr_distance = distances[&pipe];
-        let (curr_row, curr_col) = pipe;
-        let curr_type = grid[curr_row][curr_col];
-        for (adj, d_row, d_col) in get_adjacent(&grid, pipe, row_len, col_len) {
-            match (adj, curr_type, d_row, d_col) {
-                (b'|', b'7' | b'F' | b'|' | b'S', 1, 0)
-                | (b'|', b'L' | b'J' | b'|' | b'S', -1, 0)
-                | (b'-', b'L' | b'F' | b'-' | b'S', 0, 1)
-                | (b'-', b'7' | b'J' | b'-' | b'S', 0, -1)
-                | (b'L', b'7' | b'F' | b'|' | b'S', 1, 0)
-                | (b'L', b'7' | b'J' | b'-' | b'S', 0, -1)
-                | (b'J', b'7' | b'F' | b'|' | b'S', 1, 0)
-                | (b'J', b'L' | b'F' | b'-' | b'S', 0, 1)
-                | (b'7', b'L' | b'J' | b'|' | b'S', -1, 0)
-                | (b'7', b'L' | b'F' | b'-' | b'S', 0, 1)
-                | (b'F', b'L' | b'J' | b'|' | b'S', -1, 0)
-                | (b'F', b'7' | b'J' | b'-' | b'S', 0, -1) => {
-                    let new_row = (curr_row as i64 + d_row) as usize;
-                    let new_col = (curr_col as i64 + d_col) as usize;
-                    let new_pair = (new_row, new_col);
-                    let new_distance = curr_distance + 1;
-                    match distances.entry(new_pair) {
-                        Entry::Vacant(entry) => {
-                            entry.insert(new_distance);
-                            stack.push(new_pair);
-                        }
-                        Entry::Occupied(mut entry) => {
-                            if new_distance < *entry.get_mut() {
-                                entry.insert(new_distance);
-                                stack.push(new_pair);
-                            }
-                        }
-                    };
-                }
-                _ => {}
-            }
-        }
+    let mut lenght = 0;
+    while !(curr_row == start_row && curr_col == start_col) || lenght == 0 {
+        let (next_row, next_col) = grid.get_neighbor((curr_row, curr_col), (last_row, last_col));
+        (last_row, last_col) = (curr_row, curr_col);
+        (curr_row, curr_col) = (next_row, next_col);
+        lenght += 1;
     }
-
-    *distances.values().max().unwrap()
+    lenght / 2
 }
 
-fn get_adjacent(
-    grid: &[Vec<u8>],
-    (row, col): (usize, usize),
-    row_len: usize,
-    col_len: usize,
-) -> impl Iterator<Item = (u8, i64, i64)> + '_ {
-    let row = row as i64;
-    let col = col as i64;
-    let row_len = row_len as i64;
-    let col_len = col_len as i64;
-    [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        .into_iter()
-        .filter(move |(d_row, d_col)| {
-            row + d_row >= 0 && row + d_row < row_len && col + d_col >= 0 && col + d_col < col_len
-        })
-        .map(move |(d_row, d_col)| {
-            let row = (row + d_row) as usize;
-            let col = (col + d_col) as usize;
-            (grid[row][col], d_row, d_col)
-        })
-}
+pub fn part2(input: &str) -> i16 {
+    let grid = Grid::from_str(input).unwrap();
+    let (start_row, start_col) = grid.find(b'S').unwrap();
+    let (mut last_row, mut last_col) = grid.get_s_neighbors((start_row, start_col)).next().unwrap();
+    let (mut curr_row, mut curr_col) = (start_row, start_col);
 
-pub fn part2(input: &str) -> i64 {
-    let grid: Vec<_> = input.lines().map(|row| row.as_bytes().to_vec()).collect();
-    let row = grid.iter().position(|row| row.contains(&b'S')).unwrap();
-    let col = grid[row].find_byte(b'S').unwrap();
-    let row_len = grid[0].len();
-    let col_len = grid.len();
-
-    let mut stack = vec![(row, col)];
-    let mut visited = BTreeSet::new();
-    visited.insert((row, col));
-    let mut surr_loop = vec![(row as i64, col as i64)];
-
-    while let Some(pipe) = stack.pop() {
-        let (curr_row, curr_col) = pipe;
-        let curr_type = grid[curr_row][curr_col];
-        for (adj, d_row, d_col) in get_adjacent(&grid, pipe, row_len, col_len) {
-            match (adj, curr_type, d_row, d_col) {
-                (b'|', b'7' | b'F' | b'|' | b'S', 1, 0)
-                | (b'|', b'L' | b'J' | b'|', -1, 0)
-                | (b'-', b'L' | b'F' | b'-', 0, 1)
-                | (b'-', b'7' | b'J' | b'-', 0, -1)
-                | (b'L', b'7' | b'F' | b'|', 1, 0)
-                | (b'L', b'7' | b'J' | b'-', 0, -1)
-                | (b'J', b'7' | b'F' | b'|', 1, 0)
-                | (b'J', b'L' | b'F' | b'-', 0, 1)
-                | (b'7', b'L' | b'J' | b'|', -1, 0)
-                | (b'7', b'L' | b'F' | b'-', 0, 1)
-                | (b'F', b'L' | b'J' | b'|', -1, 0)
-                | (b'F', b'7' | b'J' | b'-', 0, -1) => {
-                    let new_row = (curr_row as i64 + d_row) as usize;
-                    let new_col = (curr_col as i64 + d_col) as usize;
-                    let new_pair = (new_row, new_col);
-
-                    if visited.insert(new_pair) {
-                        stack.push(new_pair);
-                        surr_loop.push((new_row as i64, new_col as i64));
-                    }
-                }
-                _ => {}
-            }
-        }
-    }
-
-    let loop_len = surr_loop.len() as i64;
-    surr_loop.push((row as i64, col as i64));
-
+    let mut lenght = 0;
     let mut twice_area = 0;
+    while !(curr_row == start_row && curr_col == start_col) || lenght == 0 {
+        let (next_row, next_col) = grid.get_neighbor((curr_row, curr_col), (last_row, last_col));
+        (last_row, last_col) = (curr_row, curr_col);
+        (curr_row, curr_col) = (next_row, next_col);
+        lenght += 1;
+        twice_area += (last_col + curr_col) * (last_row - curr_row);
+    }
+    (twice_area.abs() - lenght) / 2 + 1
+}
 
-    for ((x_0, y_0), (x_1, y_1)) in surr_loop.into_iter().tuple_windows() {
-        twice_area += (y_0 + y_1) * (x_0 - x_1);
+struct Grid<'a> {
+    inner: &'a [u8],
+    modulo: i16,
+    col_len: i16,
+}
+
+impl<'a> Grid<'a> {
+    fn from_str(input: &'a str) -> Option<Self> {
+        Some(Self {
+            inner: input.as_bytes(),
+            modulo: input.find('\n')? as i16 + 1,
+            col_len: input.lines().count() as i16,
+        })
     }
 
-    twice_area / 2 - (loop_len / 2 - 1)
+    fn find(&self, haystack: u8) -> Option<(i16, i16)> {
+        let idx = self.inner.iter().position(|&pipe| pipe == haystack)? as i16;
+        let row = idx / self.modulo;
+        let col = idx % self.modulo;
+        Some((row, col))
+    }
+
+    fn get_s_neighbors(
+        &self,
+        (curr_row, curr_col): (i16, i16),
+    ) -> impl Iterator<Item = (i16, i16)> + '_ {
+        [(-1, 0), (0, -1), (1, 0), (0, 1)]
+            .into_iter()
+            .filter(move |&(d_row, d_col)| {
+                curr_row + d_row >= 0
+                    && curr_row + d_row < self.col_len
+                    && curr_col + d_col >= 0
+                    && curr_col + d_col < self.modulo - 1
+            })
+            .map(move |(d_row, d_col)| ((d_row, d_col), self[(curr_row + d_row, curr_col + d_col)]))
+            .filter(|neighbor| {
+                matches!(
+                    neighbor,
+                    ((-1, 0), b'|' | b'7' | b'F')
+                        | ((0, -1), b'-' | b'L' | b'F')
+                        | ((1, 0), b'|' | b'L' | b'J')
+                        | ((0, 1), b'-' | b'J' | b'7')
+                )
+            })
+            .map(move |((d_row, d_col), _)| (curr_row + d_row, curr_col + d_col))
+    }
+
+    fn get_neighbor(
+        &self,
+        (curr_row, curr_col): (i16, i16),
+        (last_row, last_col): (i16, i16),
+    ) -> (i16, i16) {
+        let last_d_row = last_row - curr_row;
+        let last_d_col = last_col - curr_col;
+        match self[(curr_row, curr_col)] {
+            b'|' | b'-' => (curr_row - last_d_row, curr_col - last_d_col),
+            b'L' | b'7' => (curr_row - last_d_col, curr_col - last_d_row),
+            b'J' | b'F' => (curr_row + last_d_col, curr_col + last_d_row),
+            // (curr_row, curr_col) -> S
+            _ => self
+                .get_s_neighbors((curr_row, curr_col))
+                .find(|&pipe| pipe != (last_row, last_col))
+                .unwrap(),
+        }
+    }
+}
+
+impl Index<(i16, i16)> for Grid<'_> {
+    type Output = u8;
+
+    fn index(&self, (row, col): (i16, i16)) -> &Self::Output {
+        &self.inner[(row * self.modulo + col) as usize]
+    }
 }
