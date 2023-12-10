9d025c2b9d65740c2752b1669a59242553bd7478
Adrian Delgado <git@adrianjdelgado.com>
2023-12-09 19:59:42 -0500

    Optimize Day 6

--- src/y2023/d06.rs
+++ src/y2023/d06.rs
@@ -1,22 +1,28 @@
+use atoi::FromRadix10;
 use winnow::{
-    ascii::{alpha1, dec_uint, space1},
-    combinator::{preceded, rest, separated, separated_pair, terminated},
-    token::take_till1,
+    combinator::{preceded, rest, separated_pair, terminated},
+    token::{take, take_till1},
     PResult, Parser,
 };
 
-pub fn part1(input: &str) -> usize {
-    let (time, distance) = extract_info_1.parse(input).unwrap();
+pub fn part1(input: &str) -> u32 {
+    let (time, distance) = parse_time_distance.parse(input).unwrap();
 
-    std::iter::zip(time, distance)
-        .map(|(t, d)| (1..=t).filter(|hold| (t - hold) * hold > d).count())
+    std::iter::zip(num_iter(time), num_iter(distance))
+        .map(|(t, d)| -> u32 { (1..=t).map(|hold| u32::from((t - hold) * hold > d)).sum() })
         .product()
 }
 
-pub fn part2(input: &str) -> u64 {
-    let (t, d) = extract_info_2.parse(input).unwrap();
+pub fn part2(input: &str) -> u32 {
+    let (t, d) = parse_time_distance.parse(input).unwrap();
 
-    amount_ways_beating((t, d))
+    amount_ways_beating((parse_spaced_num(t), parse_spaced_num(d)))
+}
+
+fn num_iter(input: &str) -> impl Iterator<Item = u32> + '_ {
+    input
+        .split_ascii_whitespace()
+        .map(|n| u32::from_radix_10(n.as_bytes()).0)
 }
 
 #[allow(
@@ -24,49 +30,32 @@ pub fn part2(input: &str) -> u64 {
     clippy::cast_possible_truncation,
     clippy::cast_precision_loss
 )]
-fn amount_ways_beating((t, d): (u64, u64)) -> u64 {
+fn amount_ways_beating((t, d): (u64, u64)) -> u32 {
     let (t, d) = (t as f64, d as f64);
-    let discriminant = t * t - 4.0 * d;
+    let discriminant = t.mul_add(t, -4.0 * d);
     let start = (t - f64::sqrt(discriminant)) / 2.0;
     let end = (t + f64::sqrt(discriminant)) / 2.0;
 
-    ((end - 1.0).ceil() - (start + 1.0).floor() + 1.0) as u64
+    ((end - 1.0).ceil() - (start + 1.0).floor() + 1.0) as u32
 }
 
-fn extract_info_1(input: &mut &str) -> PResult<(Vec<u64>, Vec<u64>)> {
+fn parse_time_distance<'a>(input: &mut &'a str) -> PResult<(&'a str, &'a str)> {
     terminated(
-        separated_pair(get_several_nums, '\n', get_several_nums),
+        separated_pair(
+            preceded(take(13_usize), take_till1('\n')),
+            '\n',
+            preceded(take(12_usize), take_till1('\n')),
+        ),
         rest,
-    )
-    .parse_next(input)
-}
-
-fn get_several_nums(input: &mut &str) -> PResult<Vec<u64>> {
-    preceded(
-        (alpha1, ":", space1),
-        separated(4, dec_uint::<_, u64, _>, space1),
-    )
-    .parse_next(input)
-}
-
-fn extract_info_2(input: &mut &str) -> PResult<(u64, u64)> {
-    terminated(separated_pair(get_one_num, '\n', get_one_num), rest).parse_next(input)
-}
-
-fn get_one_num(input: &mut &str) -> PResult<u64> {
-    preceded(
-        (alpha1, ":", space1),
-        take_till1('\n').map(parse_spaced_num),
     )
     .parse_next(input)
 }
 
 fn parse_spaced_num(input: &str) -> u64 {
     input
-        .as_bytes()
-        .iter()
+        .bytes()
         .rev()
-        .filter(|&&ch| ch != b' ')
+        .filter(|&ch| ch != b' ')
         .map(|ch| u64::from(ch - b'0'))
         .zip(0..)
         .map(|(d, pos)| d * 10_u64.pow(pos))
