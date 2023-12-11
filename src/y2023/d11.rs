39caee09a47144b272c13e83e56c3cf8b3be832c
Adrian Delgado <git@adrianjdelgado.com>
2023-12-11 01:38:00 -0500

    Optimize Day 11

--- src/y2023/d11.rs
+++ src/y2023/d11.rs
@@ -1,103 +1,98 @@
-#![allow(
-    clippy::cast_possible_truncation,
-    clippy::cast_possible_wrap,
-    clippy::cast_sign_loss
-)]
+use std::ops::Index;
 
+use bstr::ByteSlice;
 use itertools::Itertools;
 
+fn solve<const EXTRA: usize>(input: &str) -> usize {
+    let grid = Grid::from_str(input).unwrap();
+    let empty_rows = grid.empty_rows();
+    let empty_cols = grid.empty_cols();
+    let galaxies = grid.galaxies();
+
+    galaxies
+        .into_iter()
+        .tuple_combinations()
+        .map(|((row_0, col_0), (row_1, col_1))| {
+            let (row_0, row_1) = sort_pair((row_0, row_1));
+            let (col_0, col_1) = sort_pair((col_0, col_1));
+            let extra_rows = empty_rows
+                .iter()
+                .filter(|&row_idx| (row_0..row_1).contains(row_idx))
+                .count()
+                * EXTRA;
+            let extra_cols = empty_cols
+                .iter()
+                .filter(|&col_idx| (col_0..col_1).contains(col_idx))
+                .count()
+                * EXTRA;
+
+            (row_1 - row_0) + extra_rows + (col_1 - col_0) + extra_cols
+        })
+        .sum()
+}
+
 pub fn part1(input: &str) -> usize {
-    let grid: Vec<_> = input.lines().map(|line| line.as_bytes()).collect();
-    let mut new_rows_idx = vec![];
-    for (row_idx, row) in grid.iter().enumerate() {
-        if row.iter().all(|&ch| ch == b'.') {
-            new_rows_idx.push(row_idx);
-        }
-    }
-    let mut new_cols_idx = vec![];
-    for col in 0..grid[0].len() {
-        if grid.iter().all(|row| row[col] == b'.') {
-            new_cols_idx.push(col);
-        }
-    }
-
-    let mut galaxies = vec![];
-    for (row_idx, row) in grid.into_iter().enumerate() {
-        for (col_idx, &ch) in row.into_iter().enumerate() {
-            if ch == b'#' {
-                galaxies.push((row_idx, col_idx));
-            }
-        }
-    }
-
-    let mut sum = 0;
-    for pair in galaxies.iter().combinations(2) {
-        let [&(x_0, y_0), &(x_1, y_1)] = &pair[..] else {
-            unreachable!()
-        };
-        let extra_rows = new_rows_idx
-            .iter()
-            .filter(|&&row_idx| {
-                (x_0 < row_idx && row_idx < x_1) || (x_1 < row_idx && row_idx < x_0)
-            })
-            .count()
-            * 1;
-        let extra_cols = new_cols_idx
-            .iter()
-            .filter(|&&col_idx| {
-                (y_0 < col_idx && col_idx < y_1) || (y_1 < col_idx && col_idx < y_0)
-            })
-            .count()
-            * 1;
-        sum += x_0.abs_diff(x_1) + y_0.abs_diff(y_1) + extra_rows + extra_cols;
-    }
-    sum
+    solve::<1>(input)
 }
 
 pub fn part2(input: &str) -> usize {
-    let grid: Vec<_> = input.lines().map(|line| line.as_bytes()).collect();
-    let mut new_rows_idx = vec![];
-    for (row_idx, row) in grid.iter().enumerate() {
-        if row.iter().all(|&ch| ch == b'.') {
-            new_rows_idx.push(row_idx);
-        }
+    solve::<999_999>(input)
+}
+
+fn sort_pair((x, y): (usize, usize)) -> (usize, usize) {
+    if x <= y {
+        (x, y)
+    } else {
+        (y, x)
     }
-    let mut new_cols_idx = vec![];
-    for col in 0..grid[0].len() {
-        if grid.iter().all(|row| row[col] == b'.') {
-            new_cols_idx.push(col);
-        }
+}
+
+struct Grid<'a> {
+    inner: &'a [u8],
+    row_len: usize,
+    col_len: usize,
+}
+
+impl<'a> Grid<'a> {
+    fn from_str(input: &'a str) -> Option<Self> {
+        Some(Self {
+            inner: input.as_bytes(),
+            row_len: input.find('\n')?,
+            col_len: input.lines().count(),
+        })
     }
 
-    let mut galaxies = vec![];
-    for (row_idx, row) in grid.into_iter().enumerate() {
-        for (col_idx, &ch) in row.into_iter().enumerate() {
-            if ch == b'#' {
-                galaxies.push((row_idx, col_idx));
-            }
-        }
+    fn empty_rows(&self) -> Vec<usize> {
+        self.inner
+            .chunks_exact(self.row_len + 1)
+            .positions(|row| row[..self.row_len].iter().all(|&ch| ch == b'.'))
+            .collect()
     }
 
-    let mut sum = 0;
-    for pair in galaxies.iter().combinations(2) {
-        let [&(x_0, y_0), &(x_1, y_1)] = &pair[..] else {
-            unreachable!()
-        };
-        let extra_rows = new_rows_idx
-            .iter()
-            .filter(|&&row_idx| {
-                (x_0 < row_idx && row_idx < x_1) || (x_1 < row_idx && row_idx < x_0)
-            })
-            .count()
-            * 999_999;
-        let extra_cols = new_cols_idx
-            .iter()
-            .filter(|&&col_idx| {
-                (y_0 < col_idx && col_idx < y_1) || (y_1 < col_idx && col_idx < y_0)
-            })
-            .count()
-            * 999_999;
-        sum += x_0.abs_diff(x_1) + y_0.abs_diff(y_1) + extra_rows + extra_cols;
+    fn empty_cols(&self) -> Vec<usize> {
+        (0..self.col_len)
+            .filter(|&col| (0..self.row_len).all(|row| self[(row, col)] == b'.'))
+            .collect()
     }
-    sum
+
+    fn galaxies(&self) -> Vec<(usize, usize)> {
+        self.inner
+            .find_iter("#")
+            .map(|idx| self.idx_to_coords(idx))
+            .collect()
+    }
+
+    fn idx_to_coords(&self, idx: usize) -> (usize, usize) {
+        let row = idx / (self.row_len + 1);
+        let col = idx % (self.row_len + 1);
+        (row, col)
+    }
+}
+
+impl Index<(usize, usize)> for Grid<'_> {
+    type Output = u8;
+
+    fn index(&self, (row, col): (usize, usize)) -> &Self::Output {
+        &self.inner[row * (self.row_len + 1) + col]
+    }
 }
