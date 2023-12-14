7493703385a736c7e989eca667fd782c55d0034d
Adrian Delgado <git@adrianjdelgado.com>
2023-12-14 12:54:02 -0500

    Optimize Day 14

--- src/y2023/d14.rs
+++ src/y2023/d14.rs
@@ -1,19 +1,17 @@
 use std::collections::HashMap;
 
+use ndarray::{Array2, ArrayView2, ArrayViewMut2};
+
 pub fn part1(input: &str) -> usize {
-    let grid: Vec<_> = input.lines().map(str::as_bytes).collect();
-    let num_rows = grid.len();
-    let num_cols = grid[0].len();
+    let num_rows = input.lines().count();
+    let num_cols = input.find('\n').unwrap();
+    let grid = ArrayView2::from_shape((num_rows, num_cols + 1), input.as_bytes()).unwrap();
 
     let mut total_load = 0;
-    for col_idx in 0..num_cols {
+    for column in grid.columns().into_iter().take(num_cols) {
         let mut next_load_amount = num_rows;
 
-        for (rock, load) in grid
-            .iter()
-            .map(|row| row[col_idx])
-            .zip((1..=num_rows).rev())
-        {
+        for (rock, load) in column.iter().zip((1..=num_rows).rev()) {
             match rock {
                 b'#' => {
                     next_load_amount = load - 1;
@@ -30,52 +28,53 @@ pub fn part1(input: &str) -> usize {
     total_load
 }
 
-const SIMULS: u32 = 1_000_000_000;
+const SIMULS: usize = 1_000_000_000;
 
 pub fn part2(input: &str) -> usize {
-    let mut grid: Vec<_> = input.lines().map(|line| line.as_bytes().to_vec()).collect();
+    let num_rows = input.lines().count();
+    let num_cols = input.find('\n').unwrap();
+    let mut grid = Array2::from_shape_fn((num_rows, num_cols), |(r, c)| {
+        input.as_bytes()[r * (num_cols + 1) + c]
+    });
 
     let mut seen = HashMap::new();
+    let mut previous_loads: Vec<Array2<u8>> = Vec::new();
 
-    for i in 0..SIMULS {
-        if let Some(last_i) = seen.insert(grid.clone(), i) {
-            // grid nuevo
-            let mut grid: Vec<_> = input.lines().map(|line| line.as_bytes().to_vec()).collect();
-            for _ in 0..(SIMULS - last_i) % (i - last_i) + last_i {
-                run_simulation(&mut grid);
-            }
-            return get_north_load(&grid);
+    for s in 0..SIMULS {
+        if let Some(last_s) = seen.insert(grid.clone(), s) {
+            let target_simul = (SIMULS - last_s) % (s - last_s) + last_s;
+            return get_north_load(previous_loads[target_simul].view());
         }
-        run_simulation(&mut grid);
+        run_simulation(grid.view_mut());
+        previous_loads.push(grid.clone());
     }
-    get_north_load(&grid)
+    get_north_load(grid.view())
 }
 
-#[allow(clippy::naive_bytecount)]
-fn get_north_load(grid: &[Vec<u8>]) -> usize {
-    grid.iter()
-        .zip((1..=grid.len()).rev())
+fn get_north_load(grid: ArrayView2<u8>) -> usize {
+    grid.rows()
+        .into_iter()
+        .zip((1..=grid.dim().0).rev())
         .map(|(row, load)| row.iter().filter(|&&ch| ch == b'O').count() * load)
         .sum()
 }
 
-fn run_simulation(grid: &mut [Vec<u8>]) {
-    let num_rows = grid.len();
-    let num_cols = grid[0].len();
+fn run_simulation(mut grid: ArrayViewMut2<u8>) {
+    let (num_rows, num_cols) = grid.dim();
 
     // north
-    for col_idx in 0..num_cols {
+    for mut column in grid.columns_mut() {
         let mut next_row_idx = 0;
 
         for row_idx in 0..num_rows {
-            let rock = grid[row_idx][col_idx];
+            let rock = column[row_idx];
             match rock {
                 b'#' => {
                     next_row_idx = row_idx + 1;
                 }
                 b'O' => {
-                    grid[row_idx][col_idx] = b'.';
-                    grid[next_row_idx][col_idx] = b'O';
+                    column[row_idx] = b'.';
+                    column[next_row_idx] = b'O';
                     next_row_idx += 1;
                 }
                 _ => {}
@@ -84,7 +83,7 @@ fn run_simulation(grid: &mut [Vec<u8>]) {
     }
 
     // west
-    for row in grid.iter_mut() {
+    for mut row in grid.rows_mut() {
         let mut next_col_idx = 0;
 
         for col_idx in 0..num_cols {
@@ -104,18 +103,18 @@ fn run_simulation(grid: &mut [Vec<u8>]) {
     }
 
     // south
-    for col_idx in 0..num_cols {
+    for mut column in grid.columns_mut() {
         let mut next_row_idx = num_rows - 1;
 
         for row_idx in (0..num_rows).rev() {
-            let rock = grid[row_idx][col_idx];
+            let rock = column[row_idx];
             match rock {
                 b'#' => {
                     next_row_idx = row_idx.saturating_sub(1);
                 }
                 b'O' => {
-                    grid[row_idx][col_idx] = b'.';
-                    grid[next_row_idx][col_idx] = b'O';
+                    column[row_idx] = b'.';
+                    column[next_row_idx] = b'O';
                     next_row_idx = next_row_idx.saturating_sub(1);
                 }
                 _ => {}
@@ -124,7 +123,7 @@ fn run_simulation(grid: &mut [Vec<u8>]) {
     }
 
     // east
-    for row in grid.iter_mut() {
+    for mut row in grid.rows_mut() {
         let mut next_col_idx = num_cols - 1;
 
         for col_idx in (0..num_cols).rev() {
