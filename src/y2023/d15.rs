ad270ac930b0d27f33bebc8aeadb5fa596704878
Adrian Delgado <git@adrianjdelgado.com>
2023-12-15 01:26:45 -0500

    Slight refactor

--- src/y2023/d15.rs
+++ src/y2023/d15.rs
@@ -1,25 +1,29 @@
+use std::iter;
+
 use winnow::{
-    ascii::{alpha1, dec_uint},
+    ascii::alpha1,
     combinator::{alt, preceded},
+    token::any,
     PResult, Parser,
 };
 
+// 511416
 pub fn part1(input: &str) -> usize {
-    input.trim_end().split(',').map(hash).sum()
+    input.trim().split(',').map(str::as_bytes).map(hash).sum()
 }
 
-fn hash(input: &str) -> usize {
-    let mut current_value = 0;
-    for ch in input.bytes() {
-        current_value += ch as usize;
-        current_value *= 17;
-        current_value %= 256;
+fn hash(input: &[u8]) -> usize {
+    let mut current_value: u8 = 0;
+    for &ch in input {
+        current_value = current_value.wrapping_add(ch);
+        current_value = current_value.wrapping_mul(17);
     }
-    current_value
+    current_value as usize
 }
 
+// 290779 202 - 215
 pub fn part2(input: &str) -> usize {
-    let mut lenses: Vec<Vec<(&str, usize)>> = Vec::with_capacity(255);
+    let mut lenses: Vec<Vec<(&[u8], usize)>> = Vec::with_capacity(255);
     for _ in 0..255 {
         lenses.push(Vec::new());
     }
@@ -27,28 +31,28 @@ pub fn part2(input: &str) -> usize {
     for ((box_idx, label), op) in input
         .trim_end()
         .split(',')
+        .map(str::as_bytes)
         .map(|op| operation.parse(op).unwrap())
     {
-        match op {
-            Op::Dash => {
-                if let Some(idx) = lenses[box_idx].iter().position(|&(b, _)| b == label) {
-                    lenses[box_idx].remove(idx);
-                }
+        let maybe_idx = lenses[box_idx].iter().position(|&(b, _)| b == label);
+        match (op, maybe_idx) {
+            (Op::Dash, Some(label_idx)) => {
+                lenses[box_idx].remove(label_idx);
             }
-            Op::Eq(focal_lenght) => {
-                if let Some(idx) = lenses[box_idx].iter().position(|&(b, _)| b == label) {
-                    lenses[box_idx][idx] = (label, focal_lenght);
-                } else {
-                    lenses[box_idx].push((label, focal_lenght))
-                }
+            (Op::Eq(focal_lenght), Some(label_idx)) => {
+                lenses[box_idx][label_idx].1 = focal_lenght;
             }
+            (Op::Eq(focal_lenght), None) => {
+                lenses[box_idx].push((label, focal_lenght));
+            }
+            _ => {}
         }
     }
 
     let mut focusing_power = 0;
 
-    for (lens_box, box_num) in lenses.into_iter().zip(1..) {
-        for ((_, focal_len), slot) in lens_box.into_iter().zip(1..) {
+    for (lens_box, box_num) in iter::zip(lenses, 1..) {
+        for ((_, focal_len), slot) in iter::zip(lens_box, 1..) {
             focusing_power += box_num * slot * focal_len;
         }
     }
@@ -62,12 +66,12 @@ enum Op {
     Eq(usize),
 }
 
-fn operation<'a>(input: &mut &'a str) -> PResult<((usize, &'a str), Op)> {
+fn operation<'a>(input: &mut &'a [u8]) -> PResult<((usize, &'a [u8]), Op)> {
     (
-        alpha1.map(|l| (hash(l), l)),
+        alpha1.map(|label| (hash(label), label)),
         alt((
-            preceded('=', dec_uint).map(|fl: u64| Op::Eq(fl as usize)),
-            '-'.value(Op::Dash),
+            preceded(b'=', any).map(|fl| Op::Eq((fl - b'0') as usize)),
+            b'-'.value(Op::Dash),
         )),
     )
         .parse_next(input)
